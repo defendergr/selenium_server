d9ae7312b731239c61817a7f14a00772af69f94e
Κωνσταντίνος Καρακασίδης <defendergr@gmail.com>
2024-03-28 12:08:10 +0200

    Πεμ 28/03/2024 script save

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run('Api.main:app', host="0.0.0.0", port=8030, reload=True)
+    uvicorn.run('Api.main:app', host="0.0.0.0", port=8030, reload=False)
